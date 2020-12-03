0f385bfaed3826b0945b1a58a600a01dfc485348
Chris Rackey <chris.rackey@gmail.com>
2020-12-03 18:47:23 -0500

    finished walk-plank user story

--- redux-pirate-crew/main.js
+++ redux-pirate-crew/main.js
@@ -8,8 +8,13 @@ const initialState = {
     {
       name: "Jack"
     }
-  ]
+  ],
+  walkedCrew: [
+  ],
+  walkedPlank: 0
 }
+
+
 
 const pirateReducer = (state = initialState, action) => {
   switch(action.type) {
@@ -18,11 +23,23 @@ const pirateReducer = (state = initialState, action) => {
       return Object.assign({}, state, {
         pirates: newPirateArray
       })
+    case WALK_PLANK:
+      const newWalkedCrewMember = state.pirates.shift()
+      const newPirateList = state.pirates
+      const walkedPlank = state.walkedPlank + 1
+      const newWalkedCrew = state.walkedCrew.concat(newWalkedCrewMember)
+      return Object.assign({}, state, {
+        pirates: newPirateList,
+        walkedCrew: newWalkedCrew,
+        walkedPlank: walkedPlank
+      })
     default:
       return state;
   }
 }
 
+const walkedCrew = document.getElementById('walked-crew')
+const plantWalkers = document.getElementById('plank-walkers')
 const newPirateForm = document.getElementById('new-pirate-form')
 
 const ADD_PIRATE = 'ADD_PIRATE'
@@ -41,17 +58,32 @@ newPirateForm.addEventListener('submit', () => {
   const newPirate = { name: name }
   store.dispatch(addPirateToList(newPirate))
 })
-
+const WALK_PLANK = 'WALK_PLANK'
+const walkPlankButton = document.getElementById('walk-the-plank')
+walkPlankButton.addEventListener('click', () => {
+  store.dispatch({
+    type: WALK_PLANK
+  })
+})
 const store = createStore(pirateReducer);
 
 const pirateList = document.getElementById('current-crew')
-
+const walkedCrewList = document.getElementById('walked-crew')
+const plantWalkersNumber = document.getElementById('plank-walkers')
 const render = () => {
   let newPirateList = ''
   store.getState().pirates.forEach(function(pirate) {
     newPirateList += `<li>${pirate.name}</li>`
   })
+  let newWalkedList = ''
+  store.getState().walkedCrew.forEach(function(pirate) {
+    newWalkedList += `<li>${pirate.name}</li>`
+  })
   pirateList.innerHTML = newPirateList
+  if(store.getState().walkedCrew.length > 0){
+    walkedCrewList.innerHTML = newWalkedList
+  }
+  plantWalkersNumber.innerHTML = store.getState().walkedPlank
 }
 
 render();
