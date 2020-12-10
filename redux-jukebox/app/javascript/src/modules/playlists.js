7dc7644f7d7a9d57236793bc6c60c94407161961
Chris Rackey <chris.rackey@gmail.com>
2020-12-09 22:49:34 -0500

    after resolving many config issues, finally made progress and finished the first user story

---
+++ redux-jukebox/app/javascript/src/modules/playlists.js
@@ -0,0 +1,78 @@
+import { displayAlertMessage } from './alertMessage.js'
+
+const initialState = {
+  artists: [],
+  artistSongs: [],
+  selectedArtistId: null,
+  playlistSongs: [],
+  isFetching: false
+}
+
+const playlists = (state = initialState, action) => {
+  switch(action.type) {
+    case GET_ARTISTS_REQUEST:
+      return {...state, isFetching: true }
+    case GET_ARTISTS_REQUEST_SUCCESS:
+      return {...state,
+        artists: action.artists,
+        isFetching: false
+      }
+    case GET_ARTISTS_REQUEST_FAILURE:
+      return {...state, isFetching: false }
+    default:
+      return state
+  }
+}
+
+
+const GET_ARTISTS_REQUEST = 'GET_ARTISTS_REQUEST'
+
+const getArtistsRequest = () => {
+  return {
+    type: GET_ARTISTS_REQUEST
+  }
+}
+
+const GET_ARTISTS_REQUEST_SUCCESS = 'GET_ARTISTS_REQUEST_SUCCESS'
+
+const getArtistsRequestSuccess = artists => {
+  return {
+    type: GET_ARTISTS_REQUEST_SUCCESS,
+    artists
+  }
+}
+
+const GET_ARTISTS_REQUEST_FAILURE = 'GET_ARTISTS_REQUEST_FAILURE'
+
+const getArtistsRequestFailure = () => {
+  return {
+    type: GET_ARTISTS_REQUEST_FAILURE
+  }
+}
+
+
+const getArtists = () => {
+  return dispatch => {
+    dispatch(getArtistsRequest())
+
+    return fetch('/api/v1/artists.json')
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+       dispatch(displayAlertMessage("Something went wrong."))
+       return { error: 'Something went wrong.' }
+      }
+    })
+    .then(artists => {
+      if(!artists.error) {
+        dispatch(getArtistsRequestSuccess(artists))
+      }
+    })
+  }
+}
+
+export {
+  playlists,
+  getArtists
+}
